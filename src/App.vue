05ded0c3963672daaa4a079c25606b17cea19085
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-18 19:19:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import EventTimer from './components/EventTimer.vue';
+
+  export default{
+    components:{
+      EventTimer,
+    }
+  }
+
+</script>
+
+<template>
+  <EventTimer />
+
+  <!-- <button class="btn btn-big ">Get started today</button>
+  <button class="btn btn-big light-orange">Get started today</button> -->
+</template>
+
+<style lang="scss">
+  @use './style/general.scss' as *;
+  @use './style/partials/variables' as *;
+</style>
---
+++ src/components/EventTimer.vue
@@ -0,0 +1,33 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <section>
+        <span class="text">Starts TOMORROW! Our biggest event of the year...</span>
+        <span class="timer">00 : 00 : 00 : 00</span>
+        <button class="btn">Get ticket</button>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    @use '../style/general.scss' as *;
+    @use '../style/partials/variables' as *;
+
+    section{
+        @include flex(center, center);
+        color: $cube-boulder;
+        background-color: $timer-bg;
+        font-size: .7rem;
+        *{
+            margin:.5rem 1rem;
+            &.timer{
+                font-size: 1rem;
+                font-weight: 600;
+                color:$cube-bay-many;
+            }
+        }
+    }
+</style>
