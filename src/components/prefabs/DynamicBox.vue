d0b26d07a61c90f3ed14d76394ee75124e3671c7
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-19 12:24:14 +0100

    add dynamicbox

---
+++ src/components/prefabs/DynamicBox.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        name:'DynamicBox',
+        props:{
+            x:Number,
+            y:Number,
+            direction:Number,
+            offsetX:Number,
+            offsetY:Number
+        },
+        computed: {
+            position(){
+                return {
+                    left : `${(this.offsetX * this.direction) + this.x}%`,
+                    top :  `${(this.offsetY * this.direction) + this.y}%`,
+                }
+            }
+        },
+    }
+</script>
+
+<template>
+    <div class="box" :style="position"></div>
+</template>
+
+<style lang="scss" scoped>
+    .box{
+        position: absolute;
+        background-color: cyan;
+        box-shadow: 5px 5px 15px 0px #0002;
+        
+        //da rimuovere
+        width: 150px;
+        height: 300px;
+    }
+</style>
