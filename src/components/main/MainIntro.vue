347b0a81acde711ca57e29116a08533a72d68c42
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-19 17:49:11 +0100

    add about me sector

--- src/components/main/MainIntro.vue
+++ src/components/main/MainIntro.vue
@@ -23,12 +23,26 @@
 
 <template>
     <section id="intro">
-        <div class="title-container">
-            <AppTitle text="Artist coaching" font-size="2rem" className="birthstone-bounce-ft"/>
-            <AppTitle text="lorem lorem lorem lorem v lorem lorem lorem lorem" font-size="1.5rem" className="quote"/>
+        <div class="sector" id="box-cards">
+            <div class="title-container">
+                <AppTitle text="Artist coaching" font-size="2rem" className="birthstone-bounce-ft"/>
+                <AppTitle text="I understand what it takes to create. I can help you" font-size="1.4rem" className="quote"/>
+            </div>
+            <div class="cards-container">
+                <ArtistBoxCard class="card" v-for="artistBox,i in artistBoxes" :index="i+1" :title="artistBox.title" :info="artistBox.info"/>
+            </div>
         </div>
-        <div class="cards-container">
-            <ArtistBoxCard class="card" v-for="artistBox,i in artistBoxes" :index="i+1" :title="artistBox.title" :info="artistBox.info"/>
+        <div class="sector" id="about-me">
+            <div class="blockquote">
+                <img src="../../assets/images/artist-quote-icon.png">
+                <AppTitle text="I coach and mentor visual artist, like you, to Create Like You Mean it" font-size="2.2rem" className="quote"/>
+                <AppTitle text="Martin Garrix" font-size="2rem" className="birthstone-bounce-ft"/>
+            </div>
+            <div class="description">
+                <p>As a coach and mentor, my ultimate goal is to motivate visual artists toward inspired action. I help you get you back on track to being the productive, creative, meaningful and authentic artist you trulu want to be. i support artists in crafting and living a creative life as well as felling good about themselves and theri art.</p>
+                <p>As an artist and an educator for nearly 30 years, I understand what it takes to create.</p>
+                <button class="btn btn-big light-orange">Get started today</button>
+            </div>
         </div>
     </section>
 </template>
@@ -36,17 +50,39 @@
 <style lang="scss" scoped>
     @use '../../style/general.scss' as *;
     section{
-        background:rebeccapurple;
-        padding:3.5rem 0;
-        .title-container{
-            @include container(35%,auto);
-            text-align: center;
-            margin-bottom: 2rem;
-        }
-        .cards-container{
+        
+        div.sector{
+            padding:3.5rem 0;
             @include container(900px, auto);
-            @include flex(flex-start, space-between);
-
+            
+            &#box-cards{
+                background:rebeccapurple;
+                .title-container{
+                    @include container(50%,auto);
+                    text-align: center;
+                }
+                .cards-container{
+                    @include flex(flex-start, space-between);
+                    padding-top:.5rem;
+                }
+            }
+            &#about-me{
+                background:magenta;
+                @include flex(center, space-between);
+                div{
+                    &.blockquote{
+                        flex-basis: calc(42% - 1rem);
+                        text-align: center;
+                    }
+                    &.description{
+                        flex-basis: calc(50% - 1rem);
+                        p{
+                            font-size: .8rem;
+                            margin-bottom: 1.5rem;
+                        }
+                    }
+                }
+            }
         }
     }
 </style>
