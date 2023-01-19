90896fe0479576c16bcac35f3c155841d61b6d16
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-19 15:59:23 +0100

    rename component AppTitle

---
+++ src/components/main/cards/ArtistBoxCard.vue
@@ -0,0 +1,33 @@
+<script>
+    import {store} from '../../../store.js'
+    export default {
+        name:'ArtistBoxCard',   
+        props:{
+            index:Number,
+            title:String,
+            info:String,
+        },
+        data() {
+            return {
+                store,
+            }
+        },
+        computed:{
+            getIndex(){
+                return (this.index < 10)? `0${this.index}` : this.index;
+            }
+        }
+    }
+</script>
+
+<template>
+    <article class="card">
+        <img :src="store.getImgPath(`artist/artist_boxes/artist-box-image-${getIndex}.png`)" :alt="`artist-box-image-${getIndex}.png`">
+        <h3>{{ title }}</h3>
+        <p>{{ info }}</p>
+    </article>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
