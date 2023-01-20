f445caeb443181d753236a0f6513c3296f231acb
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-20 13:33:03 +0100

    add luxon

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import { reactive } from "vue"
+import { DateTime } from "luxon"
 
 export const store = reactive({
     getImgPath(imgPath){
         return new URL(`./assets/images/${imgPath}`, import.meta.url).href;
     },
+    getLuxonDateTime(){
+        return DateTime;
+    }
 });
