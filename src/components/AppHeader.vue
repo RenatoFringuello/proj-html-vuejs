502fe54ca6c6b78ed36938525dda582d79d1d723
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-20 10:11:52 +0100

    add header-max-w

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -33,11 +33,12 @@
 
 <style lang="scss" scoped>
     @use '../style/general.scss' as *;
+    @use '../style/partials/variables' as *;
     header{
         background-color: red;
         padding: 1.1rem 0;
         .container{
-            @include container(1070px, auto);
+            @include container($header-max-w, auto);
             @include flex(center, space-between);
             
             ul{
